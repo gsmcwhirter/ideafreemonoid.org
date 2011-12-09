83a916cd687d26a059eeea8baa6ee4aae9f84908
Greg McWhirter <greg@hallofkvasir.org>
2011-12-09 19:24:41 +0000

    Working on getting the build notifications configured.

--- buildnotifier.js
+++ buildnotifier.js
@@ -1,35 +1,40 @@
-
 /**
  * Module dependencies.
  */
 
 var express = require('express')
-  , routes = require('./routes')
+    , config = require('./config')
+    , redis = require('redis')
+    ;
 
 var app = module.exports = express.createServer();
 
 // Configuration
 
 app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
 });
 
 app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
 app.configure('production', function(){
-  app.use(express.errorHandler()); 
+    app.use(express.errorHandler());
 });
 
 // Routes
 
-app.get('/', routes.index);
+var rclient = redis.createClient();
+
+app.get('/', function (req, res, next){
+
+});
+app.post('/', function (req, res, next){
+
+});
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
--- couchwatcher.js
+++ couchwatcher.js
@@ -1,6 +1,6 @@
 var listener = function (change){
 
-}
+};
 
 module.exports.listener = listener;
 
