1b0044f0a430f14761fa1212cc9b764523a3de56
Greg McWhirter <greg@hallofkvasir.org>
2011-12-11 21:21:08 +0000

    Adding google-spiderable routing

--- attachments/js/site.routes.js
+++ attachments/js/site.routes.js
@@ -1,5 +1,6 @@
 SC.routes.add('', Blog, Blog.Router.index);
+SC.routes.add('!blog', Blog, Blog.Router.index);
 
-SC.routes.add('cv', CV, CV.Router.index);
+SC.routes.add('!cv', CV, CV.Router.index);
 
-SC.routes.add('gametheory', Gametheory, Gametheory.Router.index);
+SC.routes.add('!gametheory', Gametheory, Gametheory.Router.index);
